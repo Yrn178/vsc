3e3bf848499693c0d26f383acaa9828358764c43
Ahosu <rybckinandrey@gmail.com>
2025-02-16 20:15:24 +0300

    create

---
+++ index.js
@@ -0,0 +1,29 @@
+const changeProposal = (arr) => {
+  const result = [];
+
+  for (const item of arr) {
+      const { word, divisibility } = item;
+      let transformedWord = '';
+
+      for (let i = 0; i < word.length; i++) {
+          if (divisibility === 'чётный' && i % 2 === 0) {
+              transformedWord += word[i].toUpperCase();
+          } else if (divisibility === 'нечётный' && i % 2 !== 0) {
+              transformedWord += word[i].toUpperCase();
+          } else {
+              transformedWord += word[i];
+          }
+      }
+
+      result.push(transformedWord);
+  }
+
+  return result;
+};
+
+console.log(changeProposal([
+  { word: 'чётный текст', divisibility: 'чётный' },
+  { word: 'нечётный текст', divisibility: 'нечётный' },
+  { word: 'этот текст тоже должен быть с каждым большим чётным символом', divisibility: 'чётный' },
+  { word: 'а у этого текста должен быть каждый нечётный большой символ', divisibility: 'нечётный' },
+]));
